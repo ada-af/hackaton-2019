b92189f78793c331aab8611d7998403e3ebbdd47
luna <luna@Moon>
2019-07-13 16:46:44 +1000

    fjsdhkfs

--- pachka_palok.py
+++ pachka_palok.py
@@ -1,9 +1,20 @@
 import jinja2
 from flask import Flask
 from flask import render_template, request, redirect
+from multiprocessing import Process
+from module import simp_filt
 
 app = Flask(__name__)
+app.config.from_pyfile('config.py', silent=True)
 
-@app.route("/")
+@app.route("/import", methods=["GET", "POST"])
 def index():
-    return "test"
+    if request.method.lower() == "get":
+        return render_template('import.html')
+    else:
+        p = Process(target=simp_filt, args=())
+        p.start()
+        p.join()
+
+
+app.run()
