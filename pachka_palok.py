8324431d0493db0ad3796e35c05247a0cc122022
luna <luna@Moon>
2019-07-13 17:19:38 +1000

    fdsjkfgws

--- pachka_palok.py
+++ pachka_palok.py
@@ -1,4 +1,7 @@
 import jinja2
+from werkzeug.utils import secure_filename
+import tempfile
+from prepare import prepare
 from flask import Flask
 from flask import render_template, request, redirect
 from multiprocessing import Process
@@ -12,7 +15,10 @@ def index():
     if request.method.lower() == "get":
         return render_template('import.html')
     else:
-        p = Process(target=simp_filt, args=())
+        file = request.files['file']
+        temp_file = secure_filename(temp_file)
+        file.save('tmp/', temp_file)
+        p = Process(target=simp_filt, args=(prepare.get_json(open('tmp/'+temp_file)))
         p.start()
         p.join()
 
