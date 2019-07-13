1bdf5d1cc79468a3399ab889836527a481562582
luna <luna@Moon>
2019-07-14 09:44:56 +1000

    89tyftdfg

--- pachka_palok.py
+++ pachka_palok.py
@@ -4,9 +4,11 @@ import tempfile
 import json
 import random
 from flask import Flask
-from flask import render_template, request, redirect
+from flask import render_template, request, redirect, make_response
 from multiprocessing import Process
 from module import prepare, simp_filt
+from hashlib import md5
+import sqlite3
 import os
 
 app = Flask(__name__)
@@ -28,6 +30,40 @@ def importer():
 @app.route("/dash", methods=['GET'])
 def dash():
     return render_template("dashboard.html", j=prepare.get_db_entry(request.remote_addr), str=str)
+
+@app.route("/sign/in", methods=['POST'])
+def sign_in():
+    if request.method.lower() == 'post':
+        user = request.form['user']
+        passwd = md5(request.form['passwd']).hexdigest()
+        conn = sqlite3.connect('sqlite.db')
+        cur = conn.cursor()
+        data = cur.execute("select count(*), user_sec from users where username='{}' and passwd='{}'".format(user, passwd))
+        if data[0][0] == 0:
+            resp = make_response(redirect('/dash'))
+            resp.set_cookie('user_sec', data[0][1])
+            return resp
+        else:
+            return redirect('/')
+
+@app.route("/sign/up", methods=["POST"])
+def sign_up():        
+    if request.method.lower() == 'post':
+        user = request.form['user']
+        passwd = md5(request.form['passwd']).hexdigest()
+        conn = sqlite3.connect('sqlite.db')
+        cur = conn.cursor()
+        user_sec = md5(str(random.randint(0, 100000))+passwd).hexdigest()
+        try:
+            cur.execute("insert into users values('{}','{}','{}'".format(user, passwd,
+            user_sec))
+        except Exception:
+            return redirect('/')
+        else:
+            resp = make_response(redirect('/dash'))
+            resp.set_cookie('user_sec', user_sec)
+            return resp
+        
 
 @app.route("/")
 def index():
